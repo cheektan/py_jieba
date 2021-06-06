cfc65ecf9a487f3c00b60527ae35041cb3a41161
cheektan <cheektan015@gmail.com>
2021-06-06 18:46:33 +0800

    all

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
 print("Default Mode: " + "/".join(seg_list))
 
 seg_list = jieba.cut("他来到网易杭研大厦")
-print(", ".join(seg_list))
+print("/".join(seg_list))
 
-seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所, 后在日本京都大学深造")
-print(", ".join(seg_list))
+seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")
+print("/".join(seg_list))
