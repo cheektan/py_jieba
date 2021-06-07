1c37fc09776333234f6fd60c47b050e0b052b099
cheektan <cheektan015@gmail.com>
2021-06-07 20:17:05 +0800

    clear up

--- Wordfrequency.py
+++ Wordfrequency.py
@@ -8,17 +8,19 @@ def main():
     file.close()
     words=jieba.lcut(text)
     counts={}
-    fW=open('two.txt','w',encoding='UTF-8')
+
+    fW=open('Wordfrequency.txt','w',encoding='UTF-8') #创建保存文件
     for word in words:
         counts[word]=counts.get(word,0) + 1
     items=list(counts.items())
     items.sort(key=takeSecond,reverse=True)
+
     for i in range(20):
         item=items[i]
         keyWord=item[0]
         count=item[1]
         print("{0:<10}{1:>5}".format(keyWord,count))
-        fW.write("{0:<5}{1:>2}".format(keyWord,count,  ))
+        fW.write("{0:<5}{1:>2}".format(keyWord,count,  )) #写入词组词频
         fW.write("\n")
     fW.close()
 main()
--- txt_jieba.py
+++ txt_jieba.py
@@ -1,12 +1,12 @@
 import jieba
 
-fR=open('doc.txt', 'r', encoding='UTF-8')
+fR=open('txt.txt', 'r', encoding='UTF-8')
 
 sent=fR.read()
 sent_list=jieba.lcut(sent)
 print(" ".join(sent_list))
 
-fW=open('outdoc.txt', 'w', encoding='UTF-8')
+fW=open('txt_out.txt', 'w', encoding='UTF-8')
 fW.write(' '.join(sent_list))
 
 fR.close()
