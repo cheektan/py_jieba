4dc54dc6059f544211dd39dcdae185a21ff1b86e
cheektan <cheektan015@gmail.com>
2021-06-12 16:55:29 +0800

    file clean

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # from jieba import analyse
 import jieba.analyse
-tfidf=jieba.analyse.extract_tags
-jieba.analyse.set_idf_path('idf.txt.big') #自定义语料
-jieba.analyse.set_stop_words('hit_stopwords.txt') #加载停用词
-text=open('res1.txt','r',encoding='utf-8').read()
-keywords=tfidf(text,topK=5)
+tfidf = jieba.analyse.extract_tags
+jieba.analyse.set_idf_path('idf.txt.big')  # 自定义语料
+jieba.analyse.set_stop_words('hit_stopwords.txt')  # 加载停用词
+text = open('txt.txt', 'r', encoding='utf-8').read()
+keywords = tfidf(text, topK=5)
 # print('keywords by tfidf:')
-f=open('z.txt','w',encoding='utf-8')
+f = open('z.txt', 'w', encoding='utf-8')
 for keyword in keywords:
     # print(keyword,'')
     f.write(keyword+'/')
--- TextRank.py
+++ TextRank.py
@@ -2,7 +2,7 @@ from jieba import Tokenizer, analyse
 textrank=analyse.textrank
 analyse.set_idf_path('idf.txt.big')
 analyse.set_stop_words('hit_stopwords.txt')
-text=open('res1.txt','r',encoding='utf-8').read()
+text=open('txt.txt','r',encoding='utf-8').read()
 print('keywords by textrank:')
 keywords=textrank(text,topK=5)
 f=open('z3.txt','w',encoding='utf-8')
